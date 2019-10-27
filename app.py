064a56e25d31d9fc15e99b082d234236ba2f32c1
xxJoJoxx <jklz521@gmail.com>
2019-10-26 20:49:16 -0500

    Inital Commit.

---
+++ app.py
@@ -0,0 +1,31 @@
+from db import init_db
+from flask import Flask
+from flask_graphql import GraphQLView
+from schema import schema
+
+app = Flask(__name__)
+app.debug = True
+
+default_query = '''
+{
+  allCustomers {
+    edges {
+      node {
+        id,
+        Vehicle {
+          id,
+          make
+        }
+      }
+    }
+  }
+}'''.strip()
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+)
+
+if __name__ == '__main__':
+    # init_db()
+    app.run(threaded=True, port=5000)
---
+++ db.py
@@ -0,0 +1,45 @@
+"""Populating the database with dummy data to start working with.
+ We can probably extend it's use to something else though"""
+
+import os
+
+from mongoengine import connect
+
+from Models.models import Vehicle, Person, UploadedImages, BaseAddress
+
+
+connect(os.getenv("MONGODB_DATABASE"), host=os.getenv("MONGODB_CONNECTION_STRING"), alias='default')
+
+
+def init_db():
+
+    # Create the fixtures
+    addr = BaseAddress(lot_num=80, street_name="Brooklyn Ave", community="Greendale", city="Spanish Town",
+                       parish="St.Catherine")
+    person_pics = UploadedImages(photo_url='www.amazons3.com/upload-test/test-image.jpg')
+    me = Person()
+    me.uuid = "123-456-789"
+    me.idType = "TRN"
+    me.photo_id = person_pics
+    me.name = 'Danielle Jackson'
+    me.age = 20
+    me.dob = '10/10/1999'
+    me.description = "An Anime lover and local weeaboo specialist"
+    me.email_address = ["dkjackson@gmail.com"]
+    me.language = ["English"]
+    me.address = addr
+    # me.photo_id.replace(customer_image, filename="man-img.png", content_type='image/jpg')
+    me.save()
+
+    vehicle_pics = UploadedImages(photo_url="www.amazons3.com/upload-test/test-image.jpg")
+    mazda = Vehicle()
+    mazda.chassis_num = "1233456677"
+    mazda.photos = [vehicle_pics]
+    mazda.model = "RX7"
+    mazda.owner_id = me.uuid
+    mazda.make = "Mazda"
+    mazda.year = "01/01/1994"
+    mazda.transmission_type = "Manual"
+    mazda.description = "Initial-D anyone?"
+    mazda.price = 1011.98
+    mazda.save()
