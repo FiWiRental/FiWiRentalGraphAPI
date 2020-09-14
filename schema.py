b405afd834329e4403b51007f185812bbf7132c6
xxJoJoxx <jklz521@gmail.com>
2020-09-14 18:44:06 -0500

    fixed git conflicts

--- schema.py
+++ schema.py
@@ -1,7 +1,6 @@
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
-<<<<<<< HEAD
 from Models.models import Vehicle as VehicleModel
 from Models.models import Person as PersonModel
 from Models.models import Rental as RentalModel
@@ -9,11 +8,6 @@ from Rentals.schema import CreateRental
 from Customers.schema import CreateCustomer
 from Vehicles.schema import CreateVehicleEntry
 
-=======
-from models import Vehicle as VehicleModel
-from models import Person as PersonModel
-from models import Rental as RentalModel
->>>>>>> development
 
 class Vehicle(MongoengineObjectType):
 
@@ -21,27 +15,18 @@ class Vehicle(MongoengineObjectType):
         model = VehicleModel
         interfaces = (Node,)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> development
 class Person(MongoengineObjectType):
 
     class Meta:
         model = PersonModel
         interfaces = (Node,)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> development
 class Rental(MongoengineObjectType):
 
     class Meta:
         model = RentalModel
         interfaces = (Node,)
 
-<<<<<<< HEAD
 
 class MyMutations(graphene.ObjectType):
     create_customer = CreateCustomer.Field()
@@ -49,22 +34,14 @@ class MyMutations(graphene.ObjectType):
     create_rental = CreateRental.Field()
 
 
-=======
->>>>>>> development
 class Query(graphene.ObjectType):
     node = Node.Field()
     all_customers = MongoengineConnectionField(Person)
     all_rentals = MongoengineConnectionField(Rental)
     all_vehicles = MongoengineConnectionField(Vehicle)
-<<<<<<< HEAD
     person = graphene.Field(Person)
     rental = graphene.Field(Rental)
     Vehicles = graphene.Field(Vehicle)
 
 
 schema = graphene.Schema(query=Query, types=[Vehicle, Person, Rental], mutation=MyMutations)
-=======
-    Vehicles = graphene.Field(Vehicle)
-
-schema = graphene.Schema(query=Query, types=[Vehicle, Person, Rental])
->>>>>>> development
